099607373f18bc1c38244698b49f5c1267f46ac0
Ayo Simon <ayosimon.dev@outlook.com>
2023-08-02 23:05:40 +0100

    first push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,13 @@ export const environment = {
   production: false,
   instagram_token: 'INSTA_TOKEN',
   stripe_token: 'STRIPE_TOKEN',
-  paypal_token: 'PAYPAL_TOKEN'
+  paypal_token: 'PAYPAL_TOKEN',
+  firebase: {
+    apiKey: 'AIzaSyCaru_NBvynMaa9ARDAXbNsS7c9gwQM_MI',
+    authDomain: 'luxury-leaf-co.firebaseapp.com',
+    projectId: 'luxury-leaf-co',
+    storageBucket: 'luxury-leaf-co.appspot.com',
+    messagingSenderId: '561357026137',
+    appId: '1:561357026137:web:9c649ae86fd31dfffe9248',
+  },
 };
